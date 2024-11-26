ccea58fda38b27a0d541fdb6ef43e7f23a4523b0
vargajanos <janosvarga706@gmail.com>
2024-11-26 10:12:16 +0100

    nagyon finom

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var session = require('express-session');
 const moment = require('moment');
 const server = http.createServer(app);
 const io = socketio(server);
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 app.use('/assets', express.static('assets'));
 
@@ -22,10 +22,24 @@ var pool = mysql.createPool({
     database : process.env.DBNAME
 });
 
+
 app.get('/', (req, res)=>{
     res.render('index.ejs');
-}); 
+});
 
+app.get('/game/:room/:user', (req, res)=>{
+    // let { name, room } = req.body;
+    session.user = req.params.user;
+    session.room = req.params.room;
+    pool.query(`SELECT * FROM questions GROUP BY RAND() LIMIT 10`, (err, results)=>{
+        if (err) {
+            console.log(err);
+            return;
+        }
+    res.render('game.ejs', {user:session.user, room:session.room, kerdesek:results});
+});
+
+});
 
 server.listen(port, ()=>{
     console.log(`Server listening on http://localhost:${port}`);
