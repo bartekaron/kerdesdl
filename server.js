6fd86826e9be8e3406174804ad936eecd88fc568
UtasiB <utasibalazs@turr.hu>
2024-11-26 09:40:21 +0100

    server finom

--- server.js
+++ server.js
@@ -2,25 +2,29 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const ejs = require('ejs');
+require("dotenv").config()
 
 const app = express();
 var session = require('express-session');
 const moment = require('moment');
 const server = http.createServer(app);
 const io = socketio(server);
+const mysql = require('mysql')
 
 app.use('/assets', express.static('assets'));
 
 var port = process.env.PORT;
 
 var pool = mysql.createPool({
-    connectionLimit : process.env.CONNECTIONLIMIT,
     host     : process.env.DBHOST,
     user     : process.env.DBUSER,
     password : process.env.DBPASSWORD,
     database : process.env.DBNAME
 });
 
+app.get('/', (req, res)=>{
+    res.render('index.ejs');
+}); 
 
 
 server.listen(port, ()=>{
